ebfb6abaf647fba3ef0e0513b62be301dc553318
arnaldorusso <arnaldorusso@gmail.com>
2014-07-28 15:30:48 -0300

    function  working

--- rollingnotes/prepare.py
+++ rollingnotes/prepare.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import re
-import numpy    
+
 
 def extractly(filename):
     """
@@ -9,8 +9,8 @@ def extractly(filename):
     Output: dict
         'music': list of lists
         'relative': initial relative tone
-    
-    Note: This program only works for compass delimiter '|', since 
+
+    Note: This program only works for compass delimiter '|', since
     it's not required by Lilypond.
     """
 
@@ -23,7 +23,7 @@ def extractly(filename):
     mus = {}
     notes = []
     for line in vector:
-        print line
+        # print line
         line = line[:-1].strip()
         if '\\relative' in line:
             mus['relative'] = line
@@ -31,9 +31,9 @@ def extractly(filename):
             mus['time'] = line
         if not line:
             pass
-        if '|' in line: # breaking compass
+        if '|' in line:  # breaking compass
             change = re.match(pattern, line)
-            if change:    
+            if change:
                 if change.group() == '\\time':
                     mus['time'] = line[6:9]
                     # This removes 'tempo' marks (3/4, 4/4, etc)
@@ -42,87 +42,108 @@ def extractly(filename):
                     notes.append(re.sub(pattern, blank, line))
             else:
                 notes.append(line)
-            
+
             compass.append(notes)
             notes = []
-            
+
         mus['music'] = compass
-        
+
     return mus
 
-def transpoly(mus):
+
+def parse_compass(mus):
+    pattern = re.compile(r".+?(?=\|)")
+    music = mus['music']
+    compass_notes = []
+    final_bar = '\\bar'
+    for compass in music:
+        text = re.match(pattern, compass[0]).group()
+        if '<' in text and '>' in text:
+            pre, rest = text.split('<')
+            inside, post = rest.split('>')
+            line = []
+            if pre:
+                [line.append(c) for c in pre.split()]
+
+            line.append(inside)
+
+            if post:
+                [line.append(p) for p in post.split()]
+
+            compass_notes.append(line)
+
+        elif final_bar in text:
+            compass_notes.append(text.split(final_bar)[0].split())
+
+        else:
+            notes = text.split()
+            compass_notes.append(notes)
+
+    return compass_notes
+
+# def _tempo(val):
+#     if val
+
+
+def transpoly(compass_notes):
     """
     It will only work for simpler lilypond files, where no fancy
     commands is present.
     """
-    start_compass = 5 # initial distance of sheet in cm
-    inc_compass = 10 # size of each compass in cm
-    inc_notes = 2
-    start_y = 20
-    pos_y = {"c": start_y,
-            "d": start_y+2,
-            "e": start_y+4,
-            "f": start_y+6,
-            "g": start_y+8,
-            "a": start_y+10,
-            "b": start_y+12,
-            "c'": start_y+14,
-            "d'": start_y+16,
-            "e'": start_y+18,
-            "f'": start_y+20,
-            "g'": start_y+22,
-            "a'": start_y+24,
-            "b'": start_y+26,
-            "c''": start_y+28}
-    pattern = re.compile(r".+?(?=\|)")
-    music = mus['music']
-    relative = mus['relative'][-4:-1]
-    tempo = mus['time']
-    tt = []
-    add = []
-    new_music = []
-    for compass in music:
-#        import ipdb; ipdb.set_trace()
-        text = re.match(pattern, compass[0]).group()
-        compass_notes = text.split()
-        for notes in compass_notes:
-            notes = notes.split()
-            for i,note in enumerate(notes):
-                print(note)
-                try:
-                    tt.append(int(note[-1]))
+    tt = {}
 
-                    if tt[i] == int(tempo[-1])*4:  # fusa
-                        add.append(inc_notes/4)
-                    if tt[i] == int(tempo[-1])*2:  # colcheia
-                        add.append(inc_notes/2)
-                    if tt[i] == int(tempo[-1]):    # seminima
-                        add.append(inc_notes)
-                    if tt[i] == int(tempo[-1])/2:  # minima
-                        add.append(inc_notes*2)
-                    if tt[i] == int(tempo[-1])/4:  # breve
-                        add.append(inc_notes*4)
-        
-                    if note[i] == "r" or "R":
-                        inc_notes += add[0]
-                        pass
-                    else:
-                        for k in pos_y.iterkeys():
-                            if k == note[0]:
-                                y_pos = pos_y[k]
-                                new_music.append((start_compass, y_pos))
+    for i, compass in enumerate(compass_notes):
+        for note in compass:
+            try:
+                tt[i] = (int(note[-1]))
+            except:
+                pass
 
-                except:
-                    pass
-                
-                start_compass += add[0]
-            add = []
-            tt = []
-            start_compass = 5
-        start_compass += inc_compass
-                
-    return new_music
-    
+#                if tt[i].append == int(tempo[-1])*4:  # fusa
+#                        add.append(inc_notes/4)
+#                    if tt[i] == int(tempo[-1])*2:  # colcheia
+#                        add.append(inc_notes/2)
+#                    if tt[i] == int(tempo[-1]):    # seminima
+#                        add.append(inc_notes)
+#                    if tt[i] == int(tempo[-1])/2:  # minima
+#                        add.append(inc_notes*2)
+#                    if tt[i] == int(tempo[-1])/4:  # breve
+#                        add.append(inc_notes*4)
+#                    if note[i] == "r" or "R":
+#                        inc_notes += add[0]
+#                        pass
+#                    else:
+#                        pass
+#                        # for k in pos_y.iterkeys():
+#                        #     if k == note[0]:
+#                        #         y_pos = pos_y[k]
+#                        #         new_music.append((start_compass, y_pos))
+#
+#                start_compass += add[0]
+#            add = []
+#            tt = []
+#            # start_compass = 5
+#        start_compass += inc_compass
+
+    # pos_y = {"c": start_y,
+    #        "d": start_y+2,
+    #        "e": start_y+4,
+    #        "f": start_y+6,
+    #        "g": start_y+8,
+    #        "a": start_y+10,
+    #        "b": start_y+12,
+    #        "c'": start_y+14,
+    #        "d'": start_y+16,
+    #        "e'": start_y+18,
+    #        "f'": start_y+20,
+    #        "g'": start_y+22,
+    #        "a'": start_y+24,
+    #        "b'": start_y+26,
+    #        "c''": start_y+28}
+
+    # return new_music
+
+
 def ly2xml():
     """
     more reading on:
